f5b6d00a173bfe9d4bb57abc3533c37c19ecdbd4
Maximo Veron <maximoveron49@gmail.com>
2025-11-10 11:01:57 -0300

    correccion de importaciones en las rutas

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,19 +1,19 @@
 import { Navigate, Route, Routes } from "react-router";
-import { Home } from "../pages/Home";
-import { Login } from "../pages/Login";
-import { Register } from "../pages/Register";
 import { PublicRoutes } from "./PublicRoutes";
 import { PrivateRoutes } from "./PrivateRoutes";
+import { LoginPage } from "../pages/LoginPage";
+import { RegisterPage } from "../pages/RegisterPage";
+import { HomePage } from "../pages/HomePage";
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route element={<PublicRoutes />}>
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
       <Route element={<PrivateRoutes />}>
-        <Route path="home" element={<Home />} />
+        <Route path="/home" element={<HomePage />} />
       </Route>
 
       <Route path="*" element={<Navigate to="/home" />} />
